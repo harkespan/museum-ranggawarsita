8943d1e421d5d6caf35d3da37f1f88f40f90820e
harkespan <octone.harry@gmail.com>
2014-11-18 22:43:31 +0700

    change service.js, adding images, change some html

--- www/js/services.js
+++ www/js/services.js
@@ -3,16 +3,40 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+/*.factory('Friends', function($http) {
+  var friends = [];
+  $http.get('http://museum-jateng.com/v2/api/koleksi')
+  .success(function(data,status,headers,config){
+    console.log('data success');
+    console.log(data);
+  })
+  .error(function(data,status,headers,config){
+    console.log('data error');
 
-  // Some fake testing data
+  })
+  .then(function(result){
+    friends = result.data;
+  });
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+})*/
+
+.factory('Friends', function() {
   var friends = [
-    { id: 0, name: 'Scruff McGruff', text : 'some text' },
-    { id: 1, name: 'G.I. Joe', text : 'some text' },
-    { id: 2, name: 'Miss Frizzle', text : 'some text' },
-    { id: 3, name: 'Ash Ketchum', text : 'some text' }
-  ];
+    {id:0,kategori:'etnografika',deskripsi:'Etnografi berasal dari 2 kata yaitu ethnos artinya bangsa, dan graphy atau grafien artinya gambaran atau uraian. Jadi, etnografi adalah uraian atau gambaran tentang bangsa-bangsa di suatu tempat dan di suatu waktu. Gambaran bangsa-bangsa tersebut meliputi adat istiadat, susunan masyarakat, bahasa, peralatan yang digunakan, aktivitas ekonominya dan gambaran fisik bangsa tersebut, misalnya warna kulit, tinggi badan, rambut, bentuk muka dan sebagainya.'},
+    {id:1,kategori:'biologika',deskripsi:'benda yang menjadi objek penelitian ilmu biologi, antara lain tengkorak atau rangka manusia, tumbuh-tumbuhan dan hewan. Misalnya burung dikeringkan'},
+    {id:2,kategori:'arkeologika',deskripsi:'Koleksi Arkeologika adalah benda yang menjadi obyek penelitian arkeologi. Benda-benda arkeologika yang ada terdiri dari kapak batu, gerabah, kalung manik, arca, dan artefak'},
+    {id:3,kategori:'historika',deskripsi:'benda yang bernilai sejarah dan menjadi objek penelitian sejarah. Benda tersebut dari sejarah masuknya budaya barat sampai dengan sekarang, misalnya Senapan laras panjang'},
+    ];
+
 
   return {
     all: function() {
@@ -27,25 +51,13 @@ angular.module('starter.services', [])
 
 .factory('Galeri',function(){
   var galeri = [
-    {id:0, pic:'pic1'},
-    {id:1, pic:'pic2'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'},
-    {id:2, pic:'pic3'}
+    {id:0, pic:'a.jpg'},
+    {id:1, pic:'b.jpg'},
+    {id:2, pic:'c.jpg'},
+    {id:3, pic:'d.jpg'},
+    {id:4, pic:'e.jpg'},
+    {id:5, pic:'f.jpg'},
+ 
   ];
 
   return {
